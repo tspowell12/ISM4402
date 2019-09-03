38f04f00ba624a6571b7cf92080437b85ced0462
tspowell12 <54450191+tspowell12@users.noreply.github.com>
2019-09-03 17:56:03 -0400

    Add files via upload

--- AncestryExcelFile.py
+++ AncestryExcelFile.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+# In[6]:
 
 
 import pandas as pd
-Location = "Your Turn/AncestryExcelFile.xls"
+Location = "Your Turn Files To Use/AncestryExcelFile.xls"
 df = pd.read_excel(Location)
 
 
-# In[5]:
+# In[7]:
 
 
 df.columns = ('AreaName', 'AreaCode', 'Ancestry0F', 'Ancestry0D', 'Ancestry0N1', 'Ancestry0N2', 'Ancestry9F', 'Ancestry9D', 'Ancestry9N1', 'Ancestry9N2')
